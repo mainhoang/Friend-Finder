e4c5c73a3f1e54124608c26fcbc3cc9d652c3ac4
Mai Hoang <mnhoang.80@gmail.com>
2017-04-25 11:36:48 -0700

    Initial site files

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+// var PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static('assests'));
+
+app.listen(3000, function(){
+	console.log("LISTENING!");
+})
