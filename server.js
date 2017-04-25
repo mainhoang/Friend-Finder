8ed79fe6c7404d5c16b355d8699131129915b3b3
Mai Hoang <mnhoang.80@gmail.com>
2017-04-25 12:14:57 -0700

    Add HTML, Bootstrap, & node_modules/

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-// var PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,6 +10,16 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(express.static('assests'));
 
-app.listen(3000, function(){
-	console.log("LISTENING!");
-})
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
+
+app.listen(3000, function() {
+    console.log("LISTENING!");
+});
