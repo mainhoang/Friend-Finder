9ab594651619ed89c87b2c6d5c53ddae8ca05344
Mai Hoang <mnhoang.80@gmail.com>
2017-04-29 09:08:52 -0700

    Solve algorithm

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,18 +11,25 @@ module.exports = function(app){
         var currFriend = req.body;
         var currScore = currFriend.score;
         console.log(currScore);
+        var best = 50;
+        var index;
 
         for( var i = 0; i < friends.length; i++){
             var diff = Math.abs(currScore - friends[i].score);
             console.log(diff);
 
+            if(diff < best){
+                best = diff;
+                index = i;
+            }
             // Got stuck on how to find best match!
 
         }
+        console.log(friends[index]);
 
         friends.push(currFriend);
         res.json(true);
-        
+
     })
 
 }
